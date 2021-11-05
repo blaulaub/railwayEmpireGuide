03919e178ef2bb0685ff62ecf10edcf08112a673
Christian Fuchs <kontaktierbar@gmail.com>
2021-11-05 19:50:12 +0100

    compute cfl

--- RailwayEmpireGuide/BalanceTransport.fsx
+++ RailwayEmpireGuide/BalanceTransport.fsx
@@ -1,5 +1,7 @@
 //module BalanceTransport
 
+open System.Collections.Generic
+
 type ProductionCapacity<'Good, 'Producer, 'Capacity> = {
     Good: 'Good
     Producer: 'Producer
@@ -64,6 +66,96 @@ let inline toDisplacements
                                 }
     }
     |> Seq.choose id
+
+let inline update1 key capacity map =
+    let v = if map |> Map.containsKey key then (map |> Map.find key) + capacity else capacity
+    map |> Map.add key v
+
+let inline update2 key1 key2 capacity map =
+    let m = if map |> Map.containsKey key1 then map |> Map.find key1 else Map.empty
+    let v = if m |> Map.containsKey key2 then (m |> Map.find key2) + capacity else capacity
+    map |> Map.add key1 (m |> Map.add key2 v)
+
+let inline accumulateDisplacements
+    (displacements: Displacement<'Good, 'Producer, 'Transporter, 'Consumer, 'Capacity> seq)
+    =
+
+    displacements
+    |> Seq.fold (fun (production, transportation, consumption) { Good = good; Producer = producer; Transporter = transporter; Consumer = consumer; Capacity = capacity } ->
+        (
+            production |> update2 good producer capacity,
+            transportation |> update1 transporter capacity,
+            consumption |> update2 good consumer capacity
+        )
+    ) (Map.empty, Map.empty, Map.empty)
+
+type CapacityPair<'Capacity> = {
+    Available: 'Capacity
+    Displaced: 'Capacity
+}
+
+let inline pairCapacities
+    (capacities: Capacities<'Good, 'Producer, 'Transporter, 'Consumer, 'Capacity>)
+    (production: Map<'Good, Map<'Producer, 'Capacity>>)
+    (transportation: Map<'Transporter, 'Capacity>)
+    (consumption: Map<'Good, Map<'Consumer, 'Capacity>>)
+    : Map<'Good, Map<'Producer, CapacityPair<'Capacity>>> * Map<'Transporter, CapacityPair<'Capacity>> * Map<'Good, Map<'Consumer, CapacityPair<'Capacity>>>
+    =
+
+    let inline add2 key1 key2 value map =
+        let m = if map |> Map.containsKey key1 then map |> Map.find key1 else Map.empty
+        map |> Map.add key1 (m |> Map.add key2 value)
+
+    capacities.ProductionCapacities
+    |> Seq.fold (fun map {Good = good; Producer = producer; Capacity = availableCapacity} ->
+        let displacedCapacity = production |> Map.find good |> Map.find producer
+        map |> add2 good producer { Available = availableCapacity; Displaced = displacedCapacity }
+    ) Map.empty
+    ,
+    capacities.TransportCapacities
+    |> Seq.fold (fun map {Producer = producer; Transporter = transporter; Consumer = consumer; Capacity = availableCapacity} ->
+        let displacedCapacity = transportation |> Map.find transporter
+        map |> Map.add transporter { Available = availableCapacity; Displaced = displacedCapacity }
+    ) Map.empty
+    ,
+    capacities.ConsumptionCapacities
+    |> Seq.fold (fun map {Good = good; Consumer = consumer; Capacity = availableCapacity} ->
+        let displacedCapacity = consumption |> Map.find good |> Map.find consumer
+        map |> add2 good consumer { Available = availableCapacity; Displaced = displacedCapacity }
+    ) Map.empty
+
+let inline computeCFL
+    (production: Map<'Good, Map<'Producer, CapacityPair<float>>>)
+    (transportation: Map<'Transporter, CapacityPair<float>>)
+    (consumption: Map<'Good, Map<'Consumer, CapacityPair<float>>>)
+    : float
+    =
+
+    seq {
+        for KeyValue(_, map) in production do
+            for KeyValue(_, pair) in map do
+                yield pair
+        for KeyValue(_, pair) in transportation do
+            yield pair
+        for KeyValue(_, map) in consumption do
+            for KeyValue(_, pair) in map do
+                yield pair
+    }
+    |> Seq.map (fun {Available = available; Displaced = displaced} ->
+        if displaced = 0. then None else Some (available/displaced)
+    )
+    |> Seq.choose id
+    |> Seq.min
+
+let inline solve (setup: CapacitiesSetup<'Good, 'Producer, 'Transporter, 'Consumer, float>)
+    =
+
+    let capacities = setup.ToCapacities
+    let displacements = capacities |> toDisplacements
+    let (m1, m2,m3) = accumulateDisplacements displacements
+    let (m4, m5, m6) = pairCapacities capacities m1 m2 m3
+    let cfl = computeCFL m4 m5 m6
+    cfl
 
 
 do  // TEST
