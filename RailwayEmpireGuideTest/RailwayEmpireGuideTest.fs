ef9aa8c5792ad7e78b16dff3fa4a1eb1cd2950a3
Christian Fuchs <kontaktierbar@gmail.com>
2022-01-01 14:02:50 +0100

    failing because nunit doesnt like static fixtures

--- RailwayEmpireGuideTest/RailwayEmpireGuideTest.fs
+++ RailwayEmpireGuideTest/RailwayEmpireGuideTest.fs
@@ -14,16 +14,22 @@ let ``can initialize world with no cities`` () =
     let world = { World.empty with Cities = [] }
     Assert.AreEqual(List.empty<City>, world.Cities)
 
+let cityOfPierre : City =
+    {
+        Name = "Pierre"
+        Population = Population 126_513
+    }
+
+let cityOfRapidCity : City =
+    {
+        Name = "Rapid City"
+        Population = Population 109_213
+    }
+
 [<Test>]
 let ``can initialize world with two cities`` () =
     // this is only an API syntax test
-    let world = {
-        World.empty with
-            Cities = [
-                { Name = "Pierre"; Population = Population 126_513 }
-                { Name = "Rapid City"; Population = Population 109_213 }
-            ]
-    }
+    let world = { World.empty with Cities = [ cityOfPierre; cityOfRapidCity ] }
     Assert.AreEqual(2, world.Cities |> List.length)
     Assert.AreEqual(1, world.Cities |> List.filter (fun city -> city.Name = "Pierre") |> List.length)
     Assert.AreEqual(1, world.Cities |> List.filter (fun city -> city.Name = "Rapid City") |> List.length)
