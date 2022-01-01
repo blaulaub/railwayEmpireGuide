934b4ae75d36e6679f89de4707b4a6a61e378aeb
Christian Fuchs <kontaktierbar@gmail.com>
2022-01-01 14:05:42 +0100

    fix failing nunit: change fixture from static to ordinary

--- RailwayEmpireGuideTest/RailwayEmpireGuideTest.fs
+++ RailwayEmpireGuideTest/RailwayEmpireGuideTest.fs
@@ -1,35 +1,38 @@
-module RailwayEmpireGuideTest
+namespace RailwayEmpireGuideTest
 
 open NUnit.Framework
 
 open RailwayEmpireGuide
 
-[<SetUp>]
-let Setup () =
-    ()
+[<TestFixture>]
+type WorldTests() =
 
-[<Test>]
-let ``can initialize world with no cities`` () =
-    // this is only an API syntax test
-    let world = { World.empty with Cities = [] }
-    Assert.AreEqual(List.empty<City>, world.Cities)
+    let cityOfPierre : City =
+        {
+            Name = "Pierre"
+            Population = Population 126_513
+        }
 
-let cityOfPierre : City =
-    {
-        Name = "Pierre"
-        Population = Population 126_513
-    }
+    let cityOfRapidCity : City =
+        {
+            Name = "Rapid City"
+            Population = Population 109_213
+        }
 
-let cityOfRapidCity : City =
-    {
-        Name = "Rapid City"
-        Population = Population 109_213
-    }
+    [<SetUp>]
+    member _.Setup () =
+        ()
 
-[<Test>]
-let ``can initialize world with two cities`` () =
-    // this is only an API syntax test
-    let world = { World.empty with Cities = [ cityOfPierre; cityOfRapidCity ] }
-    Assert.AreEqual(2, world.Cities |> List.length)
-    Assert.AreEqual(1, world.Cities |> List.filter (fun city -> city.Name = "Pierre") |> List.length)
-    Assert.AreEqual(1, world.Cities |> List.filter (fun city -> city.Name = "Rapid City") |> List.length)
+    [<Test>]
+    member _.``can initialize world with no cities`` () =
+        // this is only an API syntax test
+        let world = { World.empty with Cities = [] }
+        Assert.AreEqual(List.empty<City>, world.Cities)
+
+    [<Test>]
+    member _.``can initialize world with two cities`` () =
+        // this is only an API syntax test
+        let world = { World.empty with Cities = [ cityOfPierre; cityOfRapidCity ] }
+        Assert.AreEqual(2, world.Cities |> List.length)
+        Assert.AreEqual(1, world.Cities |> List.filter (fun city -> city.Name = "Pierre") |> List.length)
+        Assert.AreEqual(1, world.Cities |> List.filter (fun city -> city.Name = "Rapid City") |> List.length)
