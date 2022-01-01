0988ce40a3216ea541fb496564da05425eb17ebe
Christian Fuchs <kontaktierbar@gmail.com>
2022-01-01 21:53:03 +0100

    parse factory data

--- RailwayEmpireGuideTest/RailwayEmpireGuideTest.fs
+++ RailwayEmpireGuideTest/RailwayEmpireGuideTest.fs
@@ -17,8 +17,19 @@ type CityData() =
     member val Population = 0 with get, set
     member val Consumes : GoodAndAmount array = Array.empty with get, set
 
+type Throughput() =
+    member val Size = 0 with get, set
+    member val Consumes : GoodAndAmount array = Array.empty with get, set
+    member val Produces : GoodAndAmount array = Array.empty with get, set
+
+type FactoryData() =
+    member val Name = "" with get, set
+    member val Throughput : Throughput array = Array.empty with get, set
+
+
 type State() =
     member val CityData : CityData array = Array.empty with get, set
+    member val FactoryData : FactoryData array = Array.empty with get, set
 
 [<TestFixture>]
 type WorldTests() =
@@ -37,9 +48,6 @@ CityData:
     Consumes:
       - { Good: Grain, Amount: 0.1 }
       - { Good: Corn , Amount: 0.2 }
-      - { Good: Wood , Amount: 0.3 }
-      - { Good: Meat , Amount: 0.4 }
-      - { Good: Beer , Amount: 0.5 }
 """
 
         // act
@@ -49,10 +57,35 @@ CityData:
         Assert.AreEqual(1, state.CityData.Length)
         let cityData = state.CityData[0]
         Assert.AreEqual(16258, cityData.Population)
-        Assert.AreEqual(5, cityData.Consumes.Length)
+        Assert.AreEqual(2, cityData.Consumes.Length)
         let consumation = cityData.Consumes
         Assert.AreEqual(GoodAndAmount(Good= "Grain", Amount= 0.1), consumation[0])
         Assert.AreEqual(GoodAndAmount(Good= "Corn", Amount= 0.2), consumation[1])
-        Assert.AreEqual(GoodAndAmount(Good= "Wood", Amount= 0.3), consumation[2])
-        Assert.AreEqual(GoodAndAmount(Good= "Meat", Amount= 0.4), consumation[3])
-        Assert.AreEqual(GoodAndAmount(Good= "Beer", Amount= 0.5), consumation[4])
+
+    [<Test>]
+    member _.``can parse factory data in game state`` () =
+
+        // arrange
+        let input = """
+FactoryData:
+  - Name: "Breweries"
+    Throughput:
+      - Size: 1
+        Consumes: [{ Good: Grain, Amount: 0.8 }]
+        Produces: [{ Good: Beer, Amount: 1.6 }]
+"""
+
+        // act
+        let state = DeserializerBuilder().Build().Deserialize<State>(input)
+
+        // assert
+        Assert.AreEqual(1, state.FactoryData.Length)
+        let factoryData = state.FactoryData[0]
+        Assert.AreEqual("Breweries", factoryData.Name)
+        Assert.AreEqual(1, factoryData.Throughput.Length)
+        let throughput = factoryData.Throughput[0]
+        Assert.AreEqual(1, throughput.Size)
+        Assert.AreEqual(1, throughput.Consumes.Length)
+        Assert.AreEqual(GoodAndAmount(Good= "Grain", Amount= 0.8), throughput.Consumes[0])
+        Assert.AreEqual(1, throughput.Produces.Length)
+        Assert.AreEqual(GoodAndAmount(Good= "Beer", Amount= 1.6), throughput.Produces[0])
